76f0cffee506c3925a287d6231a276beb54055b8
Yazeedalyan <yazeedalyan2@gmail.com>
2022-07-27 13:53:27 +0300

    Create hellowworld.py

---
+++ hellowworld.py
@@ -0,0 +1,30 @@
+def create_youtube_video(Title,Description):
+
+	
+
+	dictionary = {"Title": Title,  "Description": Description, likes : 0 ,dislikes : 0; comments:{}}
+
+             return dictionary
+
+
+def like(youtube video,):
+	for i in dictionary :
+		if(youtube video = dictionary{i}):
+			like = like +1
+		return dictionary:
+
+
+
+
+
+
+def dislike(youtube video,Description):
+	for i in dictionary :
+		if(youtube video != dictionary{i}):
+			dislikes = dislikes +1
+		return dictionary:
+
+
+
+def dislike(youtube video,username,comment_text):
+	
